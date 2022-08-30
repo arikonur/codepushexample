7bc49a95903de9c1f467f2578e3e5620d69fbbb7
Onur Arık <62481825+arikonur@users.noreply.github.com>
2022-08-30 13:50:29 +0300

    changes navigation

--- android/app/src/main/java/com/codepushexa/MainActivity.java
+++ android/app/src/main/java/com/codepushexa/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
@@ -27,11 +28,18 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "main";
   }
+  
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegateWrapper(this,
       new ReactActivityDelegate(this, getMainComponentName())
+      {
+        @Override
+        protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+        }
     );
   }
 
